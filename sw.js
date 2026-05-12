5f8183a6fcc10fe2c8f911150713bbafd362048b
newslettersluis-del <newsletters.luis@gmail.com>
2026-05-11 21:53:19 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = 'cazador-v1';
+const ARCHIVOS = ['./index.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ARCHIVOS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
